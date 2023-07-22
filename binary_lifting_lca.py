bdcbaa55d87ff21ba0a9e7b0c4b71ad6bca83c6e
Harshit Sangwan <hsangwan941@gmail.com>
2023-07-22 18:07:46 +0530

    modified binary lifting code

--- binary_lifting_lca.py
+++ binary_lifting_lca.py
@@ -1,64 +1,58 @@
 from collections import defaultdict
-
 class TreeNode:
     def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-class Solution:
-    LOG = 8
-    MAX_N = 10e5
-    depth = defaultdict(int)
-    up = defaultdict(dict)
-
-    def dfs(self, node, parent):
-        if not node:
-            return
-
-        self.depth[node] = self.depth[parent] + 1
-
-        self.up[node][0] = parent
-        for j in range(1, self.LOG):
-            # self.up[node][j] is the 2^jth ancestor of node
-            self.up[node][j] = self.up[ self.up[node][j-1] ][j-1]
-
-        if node.left:
-            self.dfs(node.left, node)
-
-        if node.right:
-            self.dfs(node.right, node)
-
-    def LCA(self, p, q):
-        if self.depth[p] < self.depth[q]:
-            p, q = q, p
-
-        k = self.depth[p] - self.depth[q]
-
-        # make q reach the same depth as p by reaching the kth ancestor of q
-        for j in range(self.LOG-1, -1, -1):
-            if k & (1 << j):
-                p = self.up[p][j]
-
-        if p == q:
-            return p
-
-        # jump both p and q in powers of two so they get as close to the LCA as they can
-        for j in range(self.LOG-1, -1, -1):
-            if self.up[p][j] != self.up[q][j]:
-                p = self.up[p][j]
-                q = self.up[q][j]
-
-        # LCA is the parent of the node
-        return self.up[p][0]
-
-    
-    # this preprocessing is only useful if you have a tree and you have to make multiple queries on it
-    def preprocess(self, root):
-        # fill the self.up array which stores 2^jth (j in [0, LOG-1]) ancestor of every node 
-        self.dfs(root, root) # set parent as root itself to avoid indexing problems
+        val = x
+        left = None
+        right = None
 
 
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.preprocess(root)
-        return self.LCA(p, q)
+LOG = 8
+MAX_N = 10e5
+depth = defaultdict(int) # [0 for _ in range(MAX_N+1)]
+up = defaultdict(dict) # [[0 for _ in range(LOG)] for _ in range(MAX_N)]]
+
+def dfs(self, node, parent):
+    if not node:
+        return
+
+    depth[node] = depth[parent] + 1
+
+    up[node][0] = parent
+    for j in range(1, LOG):
+        # up[node][j] is the 2^jth ancestor of node
+        up[node][j] = up[ up[node][j-1] ][j-1]
+
+    if node.left:
+        dfs(node.left, node)
+
+    if node.right:
+        dfs(node.right, node)
+
+def LCA(self, p, q):
+    if depth[p] < depth[q]:
+        p, q = q, p
+
+    k = depth[p] - depth[q]
+
+    # make q reach the same depth as p by reaching the kth ancestor of q
+    for j in range(LOG-1, -1, -1):
+        if k & (1 << j):
+            p = up[p][j]
+
+    if p == q:
+        return p
+
+    # jump both p and q in powers of two so they get as close to the LCA as they can
+    for j in range(LOG-1, -1, -1):
+        if up[p][j] != up[q][j]:
+            p = up[p][j]
+            q = up[q][j]
+
+    # LCA is the parent of the node
+    return up[p][0]
+
+
+# this preprocessing is only useful if you have a tree and you have to make multiple queries on it
+def preprocess(self, root):
+    # fill the up array which stores 2^jth (j in [0, LOG-1]) ancestor of every node 
+    dfs(root, root) # set parent as root itself to avoid indexing problems
